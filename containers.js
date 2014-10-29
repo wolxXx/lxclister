506a84e559e671175ab9302c01f4422f95ee9d78
wolxXx <devops@wolxxx.de>
2014-10-29 21:05:02 +0100

    init

---
+++ containers.js
@@ -0,0 +1,23 @@
+var containers = [];
+
+containers.push({
+	'name': 'foobar',
+	'ip': '10.0.3.100',
+	'mac': '00:11:22:33:44:55',
+	list1: [
+		1,2,3,4
+	],
+	list2: [
+		42, 1337, 3.14
+	]
+});
+
+containers.push({
+	'name': 'barfoo',
+	'ip': '10.0.3.101',
+	'mac': '11:22:33:44:55:66',
+	list1: [
+		1,2,3,4
+	],
+	description: "asdf"
+});
